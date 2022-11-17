3af111348c5664b9fe94edcfbbbe78f4c9e3266e
Irina <41969451+Gingernaale@users.noreply.github.com>
2022-11-17 18:14:51 +0000

    tasks

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -1,7 +1,10 @@
 -- Find the reservation information for a customer whose
 -- name we aren't quite sure how to spell.
-
 -- Variations of the name include:
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
-
 -- There are four people in the party. Today is June 14th.
+SELECT Customers.FirstName, Customers.LastName, Reservations.Date, Reservations.PartySize
+FROM Customers 
+JOIN Reservations ON Reservations.CustomerID = Customers.CustomerID
+WHERE Customers.LastName LIKE 'St%' AND PartySize = 4
+ORDER BY Reservations.Date DESC;
