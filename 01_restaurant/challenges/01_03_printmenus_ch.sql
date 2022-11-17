932507b7310782dab7bfbc41b3d88a273264475a
Irina <41969451+Gingernaale@users.noreply.github.com>
2022-11-17 17:35:09 +0000

    tasks

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -1,5 +1,7 @@
 -- Create reports that will be used to make three menus.
-
 -- Create a report with all the items sorted by price (lowest to highest).
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
+SELECT * from dishes ORDER by Price
+SELECT Name, Description FROM Dishes WHERE Type = 'Appetizer' OR Type = 'Beverage' Order BY Type
+SELECT * from Dishes WHERE Type != 'Beverage'
--- notes.sql
+++ notes.sql
@@ -1,8 +1,3 @@
--- This file is intended for notes
--- and for demonstrating how to work
--- with SQLite in Codespaces.
-
--- These two statements operate on the 
--- restaurant.db SQLite database.
 SELECT * FROM Dishes;
 SELECT * FROM Customers;
+SELECT FirstName , LastName, Email FROM Customers, GROUP BY LastName ASC
