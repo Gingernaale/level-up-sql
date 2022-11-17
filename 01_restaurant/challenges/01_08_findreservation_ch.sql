4067345f5810633788a0dcdf1ecd640ea34d60f2
Irina <41969451+Gingernaale@users.noreply.github.com>
2022-11-17 22:21:30 +0000

    task

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -3,8 +3,14 @@
 -- Variations of the name include:
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 -- There are four people in the party. Today is June 14th.
+SELECT
+Firstname, Lastname, Date, PartySize
+from customers c, reservations r
+where c.customerid = r.customerid and c.firstname LIKE 'ST%' and PartySize = 4
+order by r.date DESC
+
 SELECT Customers.FirstName, Customers.LastName, Reservations.Date, Reservations.PartySize
 FROM Customers 
 JOIN Reservations ON Reservations.CustomerID = Customers.CustomerID
-WHERE Customers.LastName LIKE 'St%' AND PartySize = 4
+WHERE Customers.FirstName LIKE 'St%' AND PartySize = 4
 ORDER BY Reservations.Date DESC;
